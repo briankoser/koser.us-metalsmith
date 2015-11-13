3a2c49fadeb25d42e7813daab1ec75ab720841ce
Brian Koser <dev@briankoser.com>
2015-11-13 10:14:34 -0600

    Add permalinks
    "test.html" becomes "test/index.html"

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var Metalsmith = require('metalsmith'),
     inplace = require('metalsmith-in-place'),
-    markdown = require('metalsmith-markdown')
+    markdown = require('metalsmith-markdown'),
+    permalinks = require('metalsmith-permalinks'),
     serve = require('metalsmith-serve'),
-    watch = require('metalsmith-watch');;
+    watch = require('metalsmith-watch');
 
 Metalsmith(__dirname)
     .source('src')
     .destination('build')
+    .use(permalinks())
     .use(inplace({
         engine: 'handlebars',
         pattern: '**/*.md'
