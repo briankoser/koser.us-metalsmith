64adbb48a2d30203f119f5f4868355781db4fea4
Brian Koser <dev@briankoser.com>
2016-03-29 17:03:41 -0500

    Get data from page

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -6,28 +6,7 @@ function launchGallery() {
     var pswpElement = document.querySelectorAll('.pswp')[0];
 
     // build items array
-    var items = [
-        {
-            src: 'http://cdn.koser.us/pictures/2015-09-03-lydia-born/2015-09-03-lydia-born-001.jpg',
-            w: 384,
-            h: 512
-        },
-        {
-            src: 'http://cdn.koser.us/pictures/2015-09-03-lydia-born/2015-09-03-lydia-born-002.jpg',
-            w: 979,
-            h: 1306
-        },
-        {
-            src: 'http://cdn.koser.us/pictures/2015-09-03-lydia-born/2015-09-03-lydia-born-003.jpg',
-            w: 979,
-            h: 1306
-        },
-        {
-            src: 'http://cdn.koser.us/pictures/2015-09-03-lydia-born/2015-09-03-lydia-born-005.jpg',
-            w: 1306,
-            h: 979
-        }
-    ];
+    var items = JSON.parse(document.getElementById('gallery-data').innerHTML);
 
     // define options (if needed)
     var options = {
--- custom_plugins/metalsmith-photoswipe/index.js
+++ custom_plugins/metalsmith-photoswipe/index.js
@@ -36,8 +36,11 @@ function plugin(opts){
     };      
 }
 
-/** documentation
- * 1. Install plugin
- * 2. Include js, css: http://photoswipe.com/documentation/getting-started.html > Initialization > Step 1. include JS and CSS files
- * 3. 
+/** todo
+ * icon for nav bar
+ * 'y' in nav font
+ * auto get img height/width
+ * generate gallery from json
+ * generate list of galleries
+ * get thumbnails
  */
